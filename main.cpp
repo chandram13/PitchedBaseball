5eaccecb16e18eadb65bf5d619ef6163228980bc
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-29 09:58:08 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Marvish Chandra
+// Calculating the force and work of a baseball caught by a catcher
+
+#include <iostream>
+#include <cmath>
+
+class calculatingForce{
+    public static void kineticEnergy(String [] m,v){
+        kineticEnergy = 1/2 * m * (v * v);
+         std::cout << "The total kinetic energy of a baseball that is thrown by a pitcher is" << cout kineticEnergy <<  cout ".";
+    }
+    public static void baseballWork(String [] f,d,theta){
+        totalWork = f * d * cos(theta);
+        std::cout << "The total work of a baseball that is thrown by a pitcher is" << cout totalWork <<  cout ".";
+    }
+}
